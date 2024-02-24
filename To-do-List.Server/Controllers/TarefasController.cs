03ec0a226c38743f0379b3c11f6df9f204919a78
Maliniak <65911515+lucca-maliniak@users.noreply.github.com>
2024-02-23 22:02:07 -0300

    Add files via upload
    backend + bd

---
+++ To-do-List.Server/Controllers/TarefasController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using To_do_List.Server;
+using To_do_List.Server.Configuracao;
+
+namespace To_do_List.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public TarefasController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tarefas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tarefa>>> GetTarefa()
+        {
+            return await _context.Tarefa.ToListAsync();
+        }
+
+        // GET: api/Tarefas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tarefa>> GetTarefa(int id)
+        {
+            var tarefa = await _context.Tarefa.FindAsync(id);
+
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return tarefa;
+        }
+
+        // POST: api/Tarefas
+        [HttpPost]
+        public async Task<ActionResult<Tarefa>> PostTarefa([FromBody]Tarefa tarefa)
+        {
+            _context.Tarefa.Add(tarefa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTarefa", new { id = tarefa.Id }, tarefa);
+        }
+
+        // DELETE: api/Tarefas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTarefa(int id)
+        {
+            var tarefa = await _context.Tarefa.FindAsync(id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tarefa.Remove(tarefa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TarefaExists(int id)
+        {
+            return _context.Tarefa.Any(e => e.Id == id);
+        }
+    }
+}
